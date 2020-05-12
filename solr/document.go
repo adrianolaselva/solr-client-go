6198d2559710fdf0e3c27a808c625a5eca6a121f
Adriano M. La Selva <adriano.selva@picpay.com>
2020-05-12 11:38:02 -0300

    adição de comentários

--- solr/document.go
+++ solr/document.go
@@ -31,6 +31,7 @@ type DocumentAPI struct {
 	client *Client
 }
 
+// SELECT: Select documents
 func (d *DocumentAPI) Select(ctx context.Context, collection string, query string) (*Response, error) {
 	path := fmt.Sprintf("/solr/%s/select", collection)
 
@@ -49,6 +50,7 @@ func (d *DocumentAPI) Select(ctx context.Context, collection string, query strin
 	return response, err
 }
 
+// UPDATE: Update/Insert document
 func (d *DocumentAPI) Update(ctx context.Context, collection string, doc Document, params *Parameters) (*Response, error) {
 
 	path := fmt.Sprintf("/api/collections/%s/update/json", collection)
@@ -66,6 +68,7 @@ func (d *DocumentAPI) Update(ctx context.Context, collection string, doc Documen
 	return response, err
 }
 
+// UPDATE MANY: Update/Insert documents
 func (d *DocumentAPI) UpdateMany(ctx context.Context, collection string, docs []Document, params *Parameters) (*Response, error) {
 
 	path := fmt.Sprintf("/api/collections/%s/update/json", collection)
@@ -83,6 +86,7 @@ func (d *DocumentAPI) UpdateMany(ctx context.Context, collection string, docs []
 	return response, err
 }
 
+// COMMIT: Commit documents
 func (d *DocumentAPI) Commit(ctx context.Context, collection string) (*Response, error) {
 
 	path := fmt.Sprintf("/api/collections/%s/update/json", collection)
@@ -102,6 +106,7 @@ func (d *DocumentAPI) Commit(ctx context.Context, collection string) (*Response,
 	return response, err
 }
 
+// DELETE: Delete document
 func (d *DocumentAPI) Delete(ctx context.Context, collection string, delete Delete, params *Parameters) (*Response, error) {
 
 	path := fmt.Sprintf("/solr/%s/update", collection)
